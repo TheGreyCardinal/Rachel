59ad215b6533b98b05dd0d13c697858db16c29a8
tgc <thegreycardinal@gmail.com>
2020-04-20 16:39:28 +0300

    tmp Добавлен пример для проверки функционала.

---
+++ js/base.js
@@ -0,0 +1,10 @@
+/* JS-coding by Artyom Vlasov (thegreycardinal@gmail.com). */
+
+function get(el)
+ {
+  var value = el.value;
+  var out = document.getElementById("output");
+
+  out.innerHTML = value;
+ }
+
